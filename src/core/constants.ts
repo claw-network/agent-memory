cdf90844a18ea6dcde941f302e720f46d9dfef7d
weibo <benjamin-linus@outlook.com>
2026-03-26 18:32:58 +0800

    release: v0.2.0

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,5 +1,5 @@
 export const STATE_SCHEMA_VERSION = 1;
-export const GENERATOR_VERSION = "0.1.1-agent-first";
+export const GENERATOR_VERSION = "0.2.0";
 export const PROJECTION_VERSION = 2;
 export const ENTRY_VERSION = 2;
 export const VALIDATION_MAX_AGE_DAYS = 14;
